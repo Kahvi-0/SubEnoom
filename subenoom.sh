06f6b4abbb75ca9ec20646e6c02c5c7900fcc2ba
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2022-07-05 08:59:52 -0400

    Update subenoom.sh

--- subenoom.sh
+++ subenoom.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Enum Script For Subdomains and OSINT URLS"
+echo "Enum Script For Subdomains"
 
 help_screen () {
 	printf "\n HELP SCREEN \n\n"
@@ -28,7 +28,12 @@ else
 	exit 1
 fi
 
+if [[ "$*" == *"all"* ]]; then
+	echo "scan will include active methods"
 
+else
+	echo "scan will include only passive methods"
+fi
 
 #Arguments
 domain=../$1
@@ -61,9 +66,9 @@ fi
 
 cd $dir
 
-rm subdomains1.txt urls1.txt params1.txt && touch subdomains1.txt urls1.txt params1.txt
+touch subdomains1.txt params1.txt alivesubdomains2.txt alivesubdomains1.txt
 
-# Passive enum commands
+# Passive enum
 ## subdomain
 ### Amass
 echo "Amass"
@@ -74,67 +79,100 @@ amass enum -passive -df $domain -log amass.log -config $amassConfig >> subdomain
 echo "assetfinder"
 cat $domain | assetfinder --subs-only >> subdomains1.txt
 
-## Urls
+
 ### gau
+#fetches known URLs from AlienVault's Open Threat Exchange, the Wayback Machine, Common Crawl, and URLScan for any given domain
 echo "gau"
-cat $domain | gau --subs --o urls1.txt
+cat $domain | gau --subs | grep -oP '(?<=:\/\/).*?(?=\/|\?|$)' | sort | uniq >> subdomains1.txt
 
 ### waybackurls
+#Pulls urls from wayback
 echo "waybackurls"
-cat $domain | waybackurls >> urls1.txt
+cat $domain | waybackurls | grep -oP '(?<=:\/\/).*?(?=\/|\?|$)' | sort | uniq >> subdomains1.txt
+
+# Add new passive methods above me
+
 
 # Active enum 
-#brute force
-# knock
+if [[ "$*" == *"all"* ]]; then
+	echo "Active Enum"
+	#brute force
+	# knock
+
+	## Check for subdomain hijack
+	#go get github.com/haccer/subjack
+	#~/go/bin/subjack -w $url/recon/final.txt -t 100 -timeout 30 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o $url/recon/potential_takeover/PT.txt
 
 
-## Check for subdomain hijack
-#go get github.com/haccer/subjack
-#~/go/bin/subjack -w $url/recon/final.txt -t 100 -timeout 30 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o $url/recon/potential_takeover/PT.txt
+	# Add new active methods above me
+fi
 
-# Last checks 
 
-##Uniq results
-echo "sorting"
-uniq urls1.txt > urls.txt && rm urls1.txt
-## Pull parameters from results 
-cat urls.txt | grep '?*=' | cut -d '=' -f 1 | sort -u > params.txt
-
+# RESULT PARSING
 ## Pull domains from certs
 #possibly cut out-of-scope domains (tho could be useful in some cases) 
 #if any flag = -c cero will cut out of scope domains.
 if [[ "$*" == *"-c"* ]]; then
 	echo "cero respecting scope"
 	scope=$(cat $domain | tr '\n' '|')
-	cat subdomains1.txt | cero -d -c 1000 $ceroports | grep -E "($scope)" > subdomains2.txt
+	cat subdomains1.txt | sort | uniq | cero -d -c 1000 $ceroports | grep -E "($scope)" > subdomains2.txt
 else
 	echo "cero will include outofscope domains"
-	cat subdomains1.txt | cero -d -c 1000 $ceroports > subdomains2.txt
+	cat subdomains1.txt | sort | uniq | cero -d -c 1000 $ceroports > subdomains2.txt
 fi
+
 sort subdomains1.txt subdomains2.txt | uniq > subdomains.txt && rm subdomains1.txt && rm subdomains2.txt
 
 ## Check if domains are active
-echo "checking which subdomains are alive"
-cat ./subdomains.txt | httprobe $httprobeports > alivesubdomains1.txt
+if [[ "$*" == *"all"* ]]; then
+	echo "checking which subdomains are alive"
+	cat ./subdomains.txt | httprobe $httprobeports > alivesubdomains1.txt
+	sort alivesubdomains1.txt | uniq > alivesubdomains.html
+
+else
+	echo "<p><font style="color:red">Passive mode used</font></p>" > alivesubdomains.html
+fi
+
 
 #### Add option to proxy traffic through burp
-
 ## Screenshot each domain
-echo "getting screenshots"
-wget -q https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip -O aquatone.zip && mkdir tools && unzip aquatone.zip -d tools/
 
-#defaut ports "80,443,8000,8080,8443"
-#cp alivesubdomains.txt http.txt
-#cp alivesubdomains.txt https.txt
-#sed -i -e 's/^/http:\/\//' http.txt
-#sed -i -e 's/^/https:\/\//' https.txt
-#cat http.txt https.txt > screenshots1.txt
-sort alivesubdomains1.txt | uniq > alivesubdomains.txt
-cat urls.txt >> alivesubdomains1.txt
+# Final sorting
+# Ideas to check for IPs and what is inscope
+
+#Resolve domain to IP and check if resolved IP is in scope
+
+	#Tools to possibly compare for IPs
+#	filename=$(cat alivesubdomains1.txt | awk -F "/" '{print $3}')
+#	for i in $filename; do
+#		ip=$(dig +short $i)
+#		echo "$i has IP $ip"
+#		if grep -q "$i" $domain; then
+#		    echo "<p><font style="color:green">$i</font> has IP $ip </p>" >> alivesubdomains2.txt ;
+#		else
+#	 	    echo "<p><font style="color:red">$i</font> has IP $ip </p>" >> alivesubdomains2.txt ;
+#	done
+#	fi
+
+
 #removing file types that really dont need screenshots
-sort alivesubdomains1.txt | uniq | grep -v '.css\|.woff\|.js' > screenshots.txt
-cat screenshots.txt | tools/aquatone $aquatoneports -silent -out ./
+if [[ "$*" == *"all"* ]]; then
+	echo "getting screenshots of subdomains"
+	wget -q https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip -O aquatone.zip && mkdir tools && unzip -qq aquatone.zip -d tools/
+	sort alivesubdomains1.txt | uniq | grep -v '.css\|.woff\|.js' > screenshots.txt
+	cat screenshots.txt | tools/aquatone $aquatoneports -silent -out ./
+else
+	echo ""
+fi
+
+rm alivesubdomains2.txt alivesubdomains1.txt 2>&/dev/null
 
 # Output
-wget https://raw.githubusercontent.com/Kahvi-0/Pentest-Projects/master/Website%20Enumeration/Subdomain%20Enum/results.html
-firefox ./results.html ./aquatone_report.html
+wget -q https://raw.githubusercontent.com/Kahvi-0/SubEnoom/main/results.html
+
+if [[ "$*" == *"all"* ]]; then
+	firefox ./results.html ./aquatone_report.html
+
+else
+	firefox ./results.html
+fi

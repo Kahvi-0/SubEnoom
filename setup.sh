b82941374827bb61dd58c00b340727b98451a728
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2023-01-04 12:26:38 -0500

    Update and rename requirements.txt to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,8 @@
 #Installs
 
+sudo apt-get install golang-go
+
 go install github.com/tomnomnom/assetfinder@latest
 go install github.com/tomnomnom/waybackurls@latest
 go install github.com/lc/gau/v2/cmd/gau@latest
 go install github.com/tomnomnom/httprobe@latest
-
-# Config files that can be updated with APIkeys
-
-Amass:
-~/.config/amass/config.ini
-
-theHarvester:
-/etc/theHarvester/api-keys.yaml
-
-Assetfinder

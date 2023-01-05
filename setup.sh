8e750eb5a8a5e3fe6b9f985abcf4fd4422eefae9
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2023-01-05 17:18:52 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ go install github.com/tomnomnom/assetfinder@latest
 go install github.com/tomnomnom/waybackurls@latest
 go install github.com/lc/gau/v2/cmd/gau@latest
 go install github.com/tomnomnom/httprobe@latest
+go install github.com/glebarez/cero@latest

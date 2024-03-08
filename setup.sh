29b199005c8408bb0ec00ddf7b5cdaa7cee3c7b2
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2024-03-08 15:02:32 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ go install github.com/tomnomnom/waybackurls@latest
 go install github.com/lc/gau/v2/cmd/gau@latest
 go install github.com/tomnomnom/httprobe@latest
 go install github.com/glebarez/cero@latest
+go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest

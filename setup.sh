555340d7cb87a1bd2d92fca0200960d4e9fc2124
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2024-05-01 10:12:12 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #Installs
 
-sudo apt-get  -qq  install golang-go gobuster theharvester figlet
+apt-get  -qq  install golang-go gobuster theharvester figlet
 go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest
 go install github.com/tomnomnom/assetfinder@latest
 go install github.com/tomnomnom/waybackurls@latest

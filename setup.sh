924f30d1a35d5ee2a8cf1fdfd58879c60eb0407c
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2024-11-19 10:53:02 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 #Installs
 
 apt-get  -qq  install golang-go gobuster theharvester figlet
+
+# THe harvester reqs
+pip3 install ujson
+pip3 install aiomultiprocess
+pip3 install aiohttp
+pip3 install censys
+pip3 install aiodns
+pip3 install aiosqlite
+pip3 install pyppeteer
+pip3 install uvloop
+
+#
 go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest
 go install github.com/tomnomnom/assetfinder@latest
 go install github.com/tomnomnom/waybackurls@latest
@@ -10,3 +22,4 @@ go install github.com/glebarez/cero@latest
 go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install github.com/owasp-amass/amass/v4/...@master
 go install github.com/hakluke/hakrevdns@latest
+

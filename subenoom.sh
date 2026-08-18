11bf57b68fa2dba68590a384e83cf97db3ad4f08
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2026-08-18 14:31:12 -0400

    Update subenoom.sh

--- subenoom.sh
+++ subenoom.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-#==========================================
 #========= Script Setup ===================
-#==========================================
-
 WHITE="\e[97m"
 BLUE="\e[34m"
 PURPLE="\e[35m"
@@ -12,18 +8,17 @@ RED="\e[31m"
 GREEN="\e[32m"
 CYAN="\e[36m"
 ENDCOLOUR="\e[0m"
-
 # Default API file/config Locations
 amassConfig=$(pwd)/amass.config
 amassAPI=$(pwd)/amass.keys
 subfinderconfig=$(pwd)/subfinder.config
 HarvConfig=~/.theHarvester/api-keys.yaml
 
-
-
+# Tool Prep
+ports="80,443"
+dnsx -up
 #Add GO path to path to hopefully help prevent blunders
 export PATH="$HOME/go/bin:$PATH"
-
 
 help_screen() {
 	figlet HELP SCREEN
@@ -40,56 +35,7 @@ help_screen() {
   	printf "\n"
 }
 
-loadscreen() {
-
-    if [[ "$call" == 1 ]]; then
-        clear -x
-    	stats
-    	echo "Still trying to use $currentTool against $i ヾ(･ω･*)ﾉ"
-    	echo " "
-    	echo " "
-    	echo "      )  ("
-	echo "     (   ) )"
-	echo "      ) ( ("
-	echo "    _______)_"
-	echo " .-'---------|"
-	echo "( C|/\/\/\/\/|"
-	echo " '-./\/\/\/\/|"
-	echo "   '_________'"
-	echo "    '-------'"
-    	call=2
-    else
-        clear -x
-    	stats
-    	echo "Still trying to use $currentTool against $i (＾-＾)旦 "
-    	echo " "
-    	echo " "
-    	echo "      (   ) ("
-	echo "       ) (  )"
-	echo "    (         "
-	echo "    _)____(__"
-	echo " .-'---------|"
-	echo "( C|/\/\/\/\/|"
-	echo " '-./\/\/\/\/|"
-	echo "   '_________'"
-	echo "    '-------'"
-    	call=1
-    fi
-}
-
-call=1
-
-# Want to show more info about the current scan being done, look less like hanging
-stats() {
-	echo " ---------------------------------------------"
-	echo "| Output Directory: $(pwd)"
-	echo "| Current Tool: $currentTool"
-	echo "| Last updated: $(date)" 
-	echo " ----------------------------------------------"
-	echo " "
-}
-
-while getopts ":d:o:h:m:s:k:rfa" option; do
+while getopts ":d:o:h:m:s:k:p:rfa" option; do
 	case "${option}" in
 		d) # List of domains and IPs
 			domain=$(pwd)/${OPTARG};;
@@ -101,6 +47,8 @@ while getopts ":d:o:h:m:s:k:rfa" option; do
 			filter=1;;
 		a) # Skip amass?
 			amass=1;;
+		p) # port list
+			ports=${OPTARG};;
 		m) # set Amass config location
 			amassConfig=${OPTARG};;
 		s) # set subdinder config location
@@ -115,16 +63,12 @@ while getopts ":d:o:h:m:s:k:rfa" option; do
 			exit 1;;
 	esac
 done
-
 # Check for mandatory args
 if [ -z "$domain" ] || [ -z "$dir" ]; then
     help_screen
     exit 1
 fi
-
-#=======================================================
 #========= Checking existence of API files =============
-#=======================================================
 file_exist() {
 	if [ -f $FILE ]; then
 	   echo ""
@@ -149,13 +93,7 @@ rm ./amass.config
 wget -q https://raw.githubusercontent.com/Kahvi-0/SubEnoom/refs/heads/main/amass.config
 sed -i -e "s|datasources:.*$|datasources: $amassAPI|g" amass.config
 
-#Dont check for this, should be okay to run without. Cant set path through tool normally, so kinda annoying
-#FILE=$HarvConfig
-#file_exist
-
-#=======================================================
 #========= Setup directoy for output ===================
-#=======================================================
 while true; do
 	if [ ! -d "$dir" ]; then
 		mkdir $dir
@@ -170,11 +108,7 @@ while true; do
 	break
 done
 cd $dir
-
-
-#=======================================================
 #========= Setup for script options ====================
-#=======================================================
 resolveProvidedDomains() {
 	file=$(cat $domain)
 	for i in $file; do
@@ -187,7 +121,6 @@ resolveProvidedDomains() {
 		fi
 		done
 }
-
 
 # if -r is provided just add domains to InputHosts.txt
 DoNotresolveProvidedDomains() {
@@ -202,10 +135,7 @@ DoNotresolveProvidedDomains() {
 		done
 }
 
-
-#=============================================
 #========= Script Opening ====================
-#=============================================
 
 figlet Sub e Noom
 echo -e "${WHITE}[+]${BLUE} SubeNoom v4 ${WHITE}[+]${ENDCOLOR}"
@@ -263,96 +193,84 @@ domain=ExpandedScope.txt
 # inscopeips.txt = full list of provided IPs with expanded subnets
 # InputHosts.txt = domains provided 
 # ExpandedScope = All IPs and Domains provided
-
-#==========================================
 #========= ENUMERATION TOOLS ==============
-#==========================================
-
 # Reverse Lookup
-currentTool=ReverseLookup
+echo -e "1.1.1.1\n1.0.0.1\n8.8.8.8\n8.8.4.4\n9.9.9.9\n149.112.112.112\n208.67.222.222\n208.67.220.220\n94.140.14.14\n94.140.15.15" > resolvers.txt
+echo -e "\nReverse IP Lookup"
 file=$(cat inscopeips.txt)
 for i in $file; do
-        loadscreen
-	echo $i | dnsx -ptr -resp-only --silent >> ReverseIP.txt
+	echo $i | dnsx -r resolvers.txt -ptr -resp-only --silent >> ReverseIP.txt
  	echo $i | hakrevdns -r 8.8.8.8 -d >> ReverseIP.txt
 	echo $i | hakrevdns -r 1.1.1.1 -d >> ReverseIP.txt
  	echo $i | hakrevdns -r 9.9.9.9 -d >> ReverseIP.txt
    	echo $i | hakrevdns -r 208.67.222.222 -d >> ReverseIP.txt
      	echo $i | hakrevdns -r 8.26.56.26 -d >> ReverseIP.txt
-	cat ReverseIP.txt >> scan-subdomains.txt
+	cat ReverseIP.txt | sort -u >> scan-subdomains.txt
         done
-
-# AMASS
+        
+# Setup list of live sockets for other tools
+# double check that its not using the other bad httpx
+echo "HTTPX to probe web services on provided ports"
+~/go/bin/httpx -l inscopeips.txt -p $ports -silent -o hostsockets.txt
+ 
 ## Only using list of input hosts
-
-
-
 # -a arg
+echo "AMASS"
 if [[ $amass -eq 1 ]]; then
 	echo ""
 else
 	file=$(cat InputHosts.txt)
-	currentTool="Amass (can take a while)"
 	for i in $file; do
-		loadscreen
 		# Amass active + normal. -nf points the "already known subdomain names" to the provided file as to not ignore domains in the local DB
 		amass subs -d $i -config $amassConfig -names | tee -a amass.txt
                 cat amass.txt >> scan-subdomains.txt
 	done
 fi
 
-
-
-# crt.sh
-currentTool=crt.sh
+# crt.sh - Public API for registered 
+echo -e "\ncrt.sh"
 file=$(cat InputHosts.txt)
 for i in $file; do
-	loadscreen
 	curl https://crt.sh/?q=$i 2>/dev/null | grep "<TD>" | grep -v -e "style=" | sed 's/<TD>//g; s/<\/TD>//g; s/<BR>/\n/g' | tr -d ' ' | sort -u >> crtsh.txt
         cat crtsh.txt >> scan-subdomains.txt
 done
 
 # Assetfinder
-## Only using list of input hosts
+## Only using list of input hosts 
+echo -e "\nAsset Finder"
 file=$(cat InputHosts.txt)
-currentTool=Assetfinder
 for i in $file; do
-	loadscreen
 	echo $i | assetfinder --subs-only >> assetfinder.txt
         cat assetfinder.txt >> scan-subdomains.txt
 done
 
 # Gau
-currentTool=Gau
+echo -e "\nGau"
 file=$(cat InputHosts.txt)
 for i in $file; do
-	loadscreen
 	echo $i | gau --subs --blacklist png,jpg,jpeg,gif,css,svg,woff,woff2,map,pdf,js,webp,ttf,eot,webp,jfif --fc 404,400,405,500 >> gau.txt	
         cat gau.txt >> scan-urls.txt
 done
-	
 cat scan-urls.txt | grep -oP '(?<=^https:\/\/).*?(?=\/|\?|$)' | sort -u >> scan-subdomains.txt
 cat scan-urls.txt | grep -oP '(?<=^http:\/\/).*?(?=\/|\?|$)' | sort -u >> scan-subdomains.txt
 
 # Waybackurls
-currentTool=Waybackurls
+echo -e "\nWaybackurls"
 file=$(cat InputHosts.txt)
 for i in $file; do
-	loadscreen
+	
 	echo $i | waybackurls >> waybackurls.txt
 done
-
 cat waybackurls.txt | grep -oP '(?<=^https:\/\/).*?(?=\/|\?|$)' | sort -u >> scan-subdomains.txt
 cat waybackurls.txt | grep -oP '(?<=^http:\/\/).*?(?=\/|\?|$)' | sort -u >> scan-subdomains.txt
 
 # theHarvester
-currentTool=theHarvester
+echo -e "\ntheHarvester"
 filename=$(cat InputHosts.txt)
 git clone https://github.com/laramies/theHarvester
 cd theHarvester
 uv sync
 for i in $filename; do 
-	loadscreen
 	uv run theHarvester -d $i -b all > theHarvester.txt
 	cat theHarvester.txt >> theHarvester.log
 	cat theHarvester.txt | sed -n '/\[\*\] Hosts found/,$p' | awk -F ":" '{print$1}' | tail -n +3 | sort -u >> ../scan-subdomains.txt
@@ -363,108 +281,70 @@ done
 cd ..
 
 #Subfinder
-currentTool=Subfinder
+echo -e "\nSubfinder"
 subfinder -up
 filename=$(cat InputHosts.txt)
 for i in $filename; do 
-	loadscreen
 	subfinder -d $i -silent -all -pc $subfinderconfig | sort -u >> subfinder.txt
         cat subfinder.txt >> scan-subdomains.txt
 done
 
 # Brute Force with gobuster
-currentTool=gobuster
-wget https://raw.githubusercontent.com/Kahvi-0/SubEnoom/main/sublist.txt
+echo -e "\nGobuser DNS Bruteforce"s
+wget -q https://raw.githubusercontent.com/Kahvi-0/SubEnoom/main/sublist.txt
 filename=$(cat InputHosts.txt)
 for i in $filename; do
-	loadscreen
 	echo "Gobusting subdomains for $i"
-	gobuster dns -q -d $i -w sublist.txt -o gobust.txt
+	gobuster dns -q --domain $i -w sublist.txt -o gobust.txt
 	cat gobust.txt | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g' | awk -F ' ' '{print $2}' | sort | uniq >> gobusterresults.txt
         cat gobusterresults.txt >> scan-subdomains.txt
 done
 
-
-#==========================================
-#========= FINAL SORTING ==================
-#==========================================
-currentTool=Sorting
-loadscreen
+#========= SORTING ==================
 sort -u scan-subdomains.txt| grep -v "No names were discovered" > subdomains.txt
 cat scan-urls.txt | sort -u >> urls.txt
-Acat scan-emails.txt | sort -u >> emails.txt
+cat scan-emails.txt | sort -u >> emails.txt
 
-
-#===================================================================
-#========= Checking SCOPE AND ALIVE HOSTS SORTING ==================
-#===================================================================
-currentTool=httprobe
-httprobeports=""
-i="found domains and inscope IPs"
-loadscreen 
-echo "Checking which subdomains are alive"
+#========= RESOLVING IPs AND IF IN SCOPE =========
+echo -e "\nChecking which subdomains are alive"
 cat subdomains.txt inscopeips.txt | sort | uniq > alivesubdomains1.txt
 
-
-#=========================================================
-#========= RESOLVING IPs AND IF IN SCOPE =========
-#=========================================================
-
-currentTool=HOST
 filename=$(cat alivesubdomains1.txt)
-for i in $filename; do
-	loadscreen
-	if echo $i| grep -E "$WebHosting" ; then
-		:
-	else
-		host $i >> resolve1.txt ||true
-	fi
-done
-
-cat resolve1.txt | sort -u > resolve2.txt 
-cat resolve2.txt | grep -vE "$WebHosting" | grep -v "mail is handled" | grep -v '3(NXDOMAIN)' >> resolve.txt ||true
-currentTool=whois
-while read i;  do
-	loadscreen
-	if echo $i | grep -q -E '[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}$' ; then
-		ip=$(echo $i | grep address | awk -F ' ' '{print$NF}')
-		echo "$i :" $(whois $ip | sed -n 's/Organization://p' | sed  -e 's/://g') >> resolve3.txt
-	else
-		:
-	fi
-done <resolve.txt
-
-while read i;  do
-	loadscreen
+#update ########
+cat alivesubdomains1.txt InputHosts.txt | dnsx -r resolvers.txt -all -resp -silent  2>/dev/null >> resolve.txt
+while read i; do
+	echo "dnsx checkin $i"
 	if echo $i | grep -F -f inscopeips.txt; then
-	    sed -i "1s/^/<p><font style='color:purple'> $i <\/font> <\/p>\n /" alivesubdomains1.html ;
 	    echo $i >> inscopeDomains1.txt ;
 	else
- 	    echo "<p> $i </p>" >> alivesubdomains1.html ;
+ 	    if echo $i | grep -F -f InputHosts.txt; then
+ 	    	echo $i | grep -oP '\[[^\]]+\]\s+\[\K[^\]]+(?=\])' | grep -F -f InputHosts.txt 
+ 	    	echo $i | grep -oP '\[[^\]]+\]\s+\[\K[^\]]+(?=\])' | grep -F -f InputHosts.txt  >> inscopeDomains1.txt subdomains.txt
+ 	    else
+ 	    	:
+ 	    fi
 	fi
 done <resolve.txt
 
-# maybe sort by purple for all inscope at top
+filename=$(cat hostsockets.txt)
+for i in $filename; do
+	echo "Gobusting vhost for $i"
+	gobuster vhost -u $i -w subdomains.txt -o gobustvhost.txt --exclude-status "500,502,503,504" --ne --np -q -k
+	cat gobustvhost.txt | currentip=$i awk -F "Status" '{print $1, ":", ENVIRON["currentip"]}' >> vhosts-subdomains.txt
+done
 
 cat inscopeDomains1.txt | awk -F ' ' '{print$1}' | sort -u > cero.txt
 
-
-
 #cero here so it can use a list of domains found to be inscope based on provided IP
-# cero
-currentTool=cero
-ceroports=""
+ceroports="-p $ports"
 i="found domains and inscope IPs"
-loadscreen
+
 cat cero.txt inscopeips.txt | sort -u | cero -d -c 1000 $ceroports > cero-out1.txt
 cat cero-out1.txt | sort -u > cero-out.txt
 #------
 # going over inscope check again after cero
-
-currentTool=HOST-cero
 filename=$(cat cero-out.txt)
 for i in $filename; do
-	loadscreen
 	if echo $i| grep -E "$WebHosting" ; then
 		:
 	else
@@ -473,9 +353,8 @@ for i in $filename; do
 done
 cat cero1.txt | sort -u > cero2.txt 
 cat cero2.txt | grep -vE "$WebHosting"  | grep -v "mail is handled" | grep -v '3(NXDOMAIN)' >> ceroresolve.txt ||true
-currentTool=whois-cero
+
 while read i;  do
-	loadscreen
 	if echo $i | grep -q -E '[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}$' ; then
 		ip=$(echo $i | grep address | awk -F ' ' '{print$NF}')
 		echo "$i :" $(whois $ip | sed -n 's/Organization://p' | sed  -e 's/://g') >> resolve3.txt
@@ -485,31 +364,25 @@ while read i;  do
 done <ceroresolve.txt
 
 while read i;  do
-	loadscreen
 	if echo $i | grep -F -f inscopeips.txt; then
-	    sed -i "1s/^/<p><font style='color:purple'> $i <\/font> <\/p>\n /" alivesubdomains1.html ;
 	    echo $i >> inscopeDomains1.txt ;
 	else
- 	    echo "<p> $i </p>" >> alivesubdomains1.html ;
+		:
 	fi
 done <ceroresolve.txt
 
 
 #------
-cat alivesubdomains1.html | uniq > alivesubdomains.html
-cat resolve3.txt | grep "has address"| sed 's/has address/:/g' | sort -u >> ResolveFinal.txt
+cat resolve3.txt  | grep "has address"| sed 's/has address/:/g' | sort -u >> ResolveFinal.txt
 cat resolve.txt | grep "is an alias for"|  sort -u > Alias.txt
 cat inscopeDomains1.txt | grep "has address"| sed 's/has address/:/g' | sort -u > inscopeDomains.txt
 
-#==========================================
 #========= FILE CLEANUP ===================
-#==========================================
-currentTool=Filecleanup
-loadscreen
+
 #Comment line below for troubleshoooting
-rm alivesubdomains1.txt InputHosts.txt resolve.txt resolve1.txt resolve2.txt resolve3.txt scan-urls.txt scan-emails.txt sublist.txt inscopeDomains1.txt alivesubdomains1.html scan-subdomains.txt inscopeDomains1.txt
+rm alivesubdomains1.txt InputHosts.txt resolve.txt resolve1.txt resolve2.txt resolve3.txt scan-urls.txt scan-emails.txt sublist.txt inscopeDomains1.txt scan-subdomains.txt inscopeDomains1.txt
 mkdir toolOutput
-mv amass.txt assetfinder.txt cero* crtsh.txt gau.txt gobusterresults.txt hostinfo.txt ReverseIP.txt waybackurls.txt subfinder.txt theHarvester urls.txt toolOutput
+mv amass.txt assetfinder.txt cero* crtsh.txt gau.txt gobusterresults.txt hostinfo.txt ReverseIP.txt waybackurls.txt subfinder.txt theHarvester gobustvhost.txt urls.txt toolOutput
 
 awk -F':' '
 {
@@ -534,7 +407,9 @@ END {
     print ip[i] "\n"
   }
 }
-' inscopeDomains.txt > CleanedDomains.txt
+' inscopeDomains.txt > CleanedDomains1.txt
+
+cat CleanedDomains1.txt vhosts-subdomains.txt | sort -u > CleanedDomains.txt
 
 #Final CLI output file
 echo "#=============================================" > Finalout.txt
@@ -557,13 +432,7 @@ echo "#================= Alias' ====================" >> Finalout.txt
 echo "#=============================================" >> Finalout.txt
 cat Alias.txt >> Finalout.txt
 
-#==========================================
 #========= Output =========================
-#==========================================
-
-wget -q https://raw.githubusercontent.com/Kahvi-0/SubEnoom/main/results.html
-firefox ./results.html 2> /dev/null &
-
 echo " "
 figlet output
 cat Finalout.txt
@@ -580,7 +449,8 @@ wc -l toolOutput/gau.txt | awk -F " " '{print "Gau Results: " $1}'
 wc -l toolOutput/waybackurls.txt | awk -F " " '{print "Waybackurls Results: " $1}'
 wc -l toolOutput/theHarvester/theHarvester.txt | awk -F " " '{print "theHarvester Results: " $1}'
 wc -l toolOutput/subfinder.txt | awk -F " " '{print "subfinder Results: " $1}'
-wc -l toolOutput/gobusterresults.txt | awk -F " " '{print "GoBuster Results: " $1}'
+wc -l toolOutput/gobusterresults.txt | awk -F " " '{print "GoBuster DNS Bust Results: " $1}'
+wc -l toolOutput/gobustvhost.txt | awk -F " " '{print "GoBuster vhost Results: " $1}'
 wc -l toolOutput/cero-out.txt | awk -F " " '{print "Cero Results: " $1}'
 
 
